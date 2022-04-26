80a48ade15a8dc1f248dc086673199549e520914
--list <ferdinand@familie-campe.de>
2022-04-26 13:36:18 +0200

    some changes

--- src/extrafile.jl
+++ src/extrafile.jl
@@ -1,1 +1,3 @@
-two_times_x_plus_y(x, y) = 2*x+y
+two_times_x_plus_y(x, y) = two_times_x(x) + y
+
+two_times_x(x) = 2*x
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "MyExample.jl" begin
     @test two_times_x_plus_y(2,1) == 5
-    @test MyExample.two_times_x_plus_y(3,1) == 7
-    @test MyExample.two_times_x_plus_y(2,2) == 6
+    @test two_times_x_plus_y(3,1) == 7
+    @test two_times_x_plus_y(2,2) == 6
     # Write your tests here.
 end
